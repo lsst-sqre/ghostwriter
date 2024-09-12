4cb97781f31f6861e34ea9340e069442187948e1
adam <athornton@lsst.org>
2024-09-11 23:32:38 -0700

    Externalize GH notebook payload so we can lint it

--- src/ghostwriter/hooks/__init__.py
+++ src/ghostwriter/hooks/__init__.py
@@ -1,15 +1,11 @@
 from .ensure_lab import ensure_running_lab
 from .github_notebook import github_notebook
 from .portal_query import portal_query
-from .system_test import system_test
-from .tutorial import tutorial_on_demand
 from .vacuous import vacuous_hook
 
 __all__ = [
     "ensure_running_lab",
     "github_notebook",
     "portal_query",
-    "tutorial_on_demand",
-    "system_test",
     "vacuous_hook",
 ]
--- src/ghostwriter/hooks/github_notebook.py
+++ src/ghostwriter/hooks/github_notebook.py
@@ -5,6 +5,7 @@ the hooks.
 """
 
 import inspect
+from pathlib import Path
 from string import Template
 from urllib.parse import urljoin
 
@@ -68,68 +69,5 @@ def _get_code_from_template(client_path: str) -> str:
 
 def _get_nbcheck_template() -> str:
     return inspect.cleandoc(
-        """import os
-        import requests
-        from base64 import b64decode
-        from datetime import datetime, timezone
-        from pathlib import Path
-
-        path = "${path}"
-        branch = ""
-        branch_components = path.split("@")
-        if len(branch_components) > 2:
-            raise RuntimeError(f"more than one '@' found on path '{path}'")
-        if len(branch_components) == 2:
-            path = branch_components[0]
-            branch = branch_components[1]
-        path_components = path.split("/")
-        if len(path_components) < 4:
-            raise RuntimeError(f"host/owner/repo/file not found in '{path}'")
-        host = path_components[0]
-        owner = path_components[1]
-        repo = path_components[2]
-        rest = "/".join(path_components[3:])
-        # Filter by allowed hosts--just "github.com", because we use the GH
-        # API
-        if host != "github.com":
-            raise RuntimeError(f"'{host}' not 'github.com'")
-        # Filter by allowed owning organizations
-        allowed_owners = ("lsst", "lsst-dm", "lsst-sqre",
-                          "lsst-ts", "rubin-dp0")
-        if owner not in allowed_owners:
-            raise RuntimeError(f"{owner} not in {allowed}")
-        # Canonicalize path
-        if path.endswith(".ipynb"):
-            path=path[:-(len(".ipynb"))]
-            rest=rest[:-(len(".ipynb"))]
-        topdir = Path(os.environ['HOME']) / "notebooks" / "on-demand"
-        nbdir = (topdir / path).parent
-        nb_base = Path(path).name
-        nb = Path(nbdir / f"{nb_base}.ipynb")
-        nbdir.mkdir(exist_ok=True, parents=True)
-        serial = 0
-        while nb.exists():
-            # Count up until we find an unused number to append to the name.
-            serial += 1
-            nb = nbdir / f"{nb_base}-{serial}.ipynb"
-
-        # Retrieve notebook content from github.
-        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{rest}"
-        url += ".ipynb"
-        if branch:
-            url += f"?ref={branch}"
-        r=requests.get(url)
-        obj=r.json()
-        content_b64 = obj["content"]
-        # Turn that back into a UTF-8 string
-        content = b64decode(content_b64).decode()
-
-        # And write it into place
-        nb.write_text(content)
-
-        # Finally, print the value of ``serial``, which we will capture as
-        # a notebook stream output to determine whether we need to modify
-        # the path and unique_id in rewrite parameters.
-        print(serial)
-        """
+        (Path(__file__).parent / "_github_notebook_payload.py").read_text()
     )
